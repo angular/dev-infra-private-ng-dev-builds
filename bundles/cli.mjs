0e5056c7c5d6cb26209a8391c6539a4179256911
Angular Robot <angular-robot@google.com>
2022-08-03 21:50:01 +0000

    main - d2f9d2f build: update angular dependencies (#751)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -73206,7 +73206,7 @@ import * as fs3 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a, _b, _c;
-  const localVersion = `0.0.0-8fa614960c2b5950438d4d33292ac16c23180d77`;
+  const localVersion = `0.0.0-d2f9d2f493ec99f89a1db2777e2b27c1f5b278e9`;
   const workspacePackageJsonFile = path2.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path2.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
