0cb02ba1aa76c7071c5046fb068b64bf6d6b3d26
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-09-23 16:49:23 +0000

    main - 989b9f5 feat(circleci-orb): command for setting up remote bazel execution (#839)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -92184,7 +92184,7 @@ import * as fs3 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-16a0ad3592bea9d6cab6297a85d98f87b8fae30c`;
+  const localVersion = `0.0.0-989b9f572138afc4d89d7489cc19a39919b056ef`;
   const workspacePackageJsonFile = path2.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path2.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
