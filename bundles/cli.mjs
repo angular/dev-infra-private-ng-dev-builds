e289031ccd3b04feecc74531b3e2d8aa40cc3c92
Angular Robot <angular-robot@google.com>
2022-11-21 17:00:21 +0000

    main - ca1c78c build: update dependency firebase-functions to v4 (#874)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93740,7 +93740,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-8bfe54b44dc7e08901b49ebf891a8d5fbcff3a54`;
+  const localVersion = `0.0.0-ca1c78cb3551ddadcc0538ba0441f31f3bcad323`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
