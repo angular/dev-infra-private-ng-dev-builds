4e629002ff0209a984e9e795a424c078c625c8f1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-11-29 15:15:46 +0000

    main - 49d2afa fix(github-actions): do not attempt to join path with

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93740,7 +93740,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-d30e42411eee61f75e39cb39ffd5e1c7946a7ed5`;
+  const localVersion = `0.0.0-49d2afadffb93beaf0753014ca0bbfa8b3a5c9b0`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
