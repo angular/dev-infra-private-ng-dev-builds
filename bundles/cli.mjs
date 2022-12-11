3c7d16f4929bc9ef10441bf17f327f6e0b32eee5
Angular Robot <angular-robot@google.com>
2022-12-11 00:12:59 +0000

    main - 6febf83 release: create weekly changelog entry

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93750,7 +93750,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-96fdaaa056f1cfa7ffbc4c69b7e9007279f76c94`;
+  const localVersion = `0.0.0-6febf83cd9676628d146d4ff692e73dab2f53853`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
