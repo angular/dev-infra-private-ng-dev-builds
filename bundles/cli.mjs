858c8c564ce30f9f0658b258f7b8357ef88b8f82
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-12 17:49:26 +0000

    main - 57b5fea fix(shared-scripts): properly use `isSideEffectFree` function

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93750,7 +93750,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-7246a9c004fe00231816f71c6433df329d8e6c1f`;
+  const localVersion = `0.0.0-57b5fea0ace8cee1dff586d192383ddf00576b31`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
