d5f98f0996ca6c04d5e6963031730173fad0ef18
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-14 13:27:26 +0000

    main - d4d1329 feat(bazel): allow for benchmark driver utilities to be linked

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93750,7 +93750,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-bc7bf8bfc84492f04d18dc1cbf4b7547e51c3786`;
+  const localVersion = `0.0.0-d4d1329adbe1cdb5fd382d618e830c69e578c41b`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
