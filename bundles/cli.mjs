23134bb3a3dfd31831c5f67a12221263f4940d86
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 21:27:01 +0000

    main - 1cc2f89 build: fix scorecard action failing (#963)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93902,7 +93902,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-13cc1c7b2ac7cec0d6a41c1d5bcf8d593c42f6b6`;
+  const localVersion = `0.0.0-1cc2f89cfa4e735f2a0bcf2af740ab364e2a27bb`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
