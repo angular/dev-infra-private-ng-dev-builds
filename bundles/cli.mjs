a2a7f9630a3632a648f2deea168b0274a5c116eb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-05 15:07:41 +0000

    main - 67b18c4 build: change snapshot build token variable to `SNAPSHOT_BUILDS_GITHUB_TOKEN`

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93902,7 +93902,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-aebbf51707cefa7d1fa1294033e73213c8845e28`;
+  const localVersion = `0.0.0-67b18c484f17f0f23be5a5592ab9d84ebd203a6d`;
   const workspacePackageJsonFile = path3.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path3.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
