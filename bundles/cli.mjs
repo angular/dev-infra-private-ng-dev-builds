655c5fdf0d9f72f60ef41d7bd9e2c4efb1ec3749
Joey Perrott <josephperrott@gmail.com>
2023-03-30 17:35:11 +0000

    main - 4d96664b fix(github-actions): update lock-closed inputs (#1083)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93436,7 +93436,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-dc1e7daff187d3142ba4a73f1ced943f9778d265`;
+  const localVersion = `0.0.0-4d96664b8fe4c1e8fbd51ffe89f402bd7e003dbf`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
