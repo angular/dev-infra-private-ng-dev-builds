c7a1f8f1d6af648ac22f48dc4d86ed893543beb4
Angular Robot <angular-robot@google.com>
2023-03-30 17:14:08 +0000

    main - b46006ec build: update scorecard action dependencies (#1071)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93436,7 +93436,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-531db2a74e02fc2bbee2202d4bc3e9814c6c1157`;
+  const localVersion = `0.0.0-b46006ecbb87e9784f0967e8b25ba538bee2fe1b`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
