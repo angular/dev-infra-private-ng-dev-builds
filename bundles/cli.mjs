a3045dc8deda30bc14821b82df054e78c15eb533
Joey Perrott <josephperrott@gmail.com>
2023-04-21 14:25:44 +0000

    main - 3f26e9ec feat(apps): create the code of conduct managing functions (#1109)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -93607,7 +93607,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-08f8d677da7b0811a00e2898596ef329e3450c88`;
+  const localVersion = `0.0.0-3f26e9eca758ddbd54b744c03f25104c54b0811c`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
