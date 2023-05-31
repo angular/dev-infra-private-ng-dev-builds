86eaf4186077a69891085ca2e015f2cb7ded8281
Angular Robot <angular-robot@google.com>
2023-05-31 13:23:39 +0000

    main - c19e369f build: update dependency @octokit/graphql-schema to v14.6.0 (#1191)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -79564,7 +79564,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-2ec9a17752d4f75710814138e805222aa194e1b6`;
+  const localVersion = `0.0.0-c19e369fad9e106c9a64859407565e21441f4ddf`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
