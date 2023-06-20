6ba67f870c979b135569dbd51059448cd5a1b1de
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-20 12:57:35 +0000

    main - 6ef015b7 fix(bazel): spec bundling and app bundling breaking rxjs v6 testing code

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -79294,7 +79294,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-8d4803573edc70b90a1134ffa996303d1dcc18a9`;
+  const localVersion = `0.0.0-6ef015b7ab6d31ecf452c735ee9f397c36f08033`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
