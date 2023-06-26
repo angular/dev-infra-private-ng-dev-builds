6c238d3a07c08c79b4f995c764ac9e0fb07dd141
Joey Perrott <josephperrott@gmail.com>
2023-06-26 14:50:40 +0000

    main - 8969d1fd ci: include bazel output/cache directories in action caching (#1234)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -79294,7 +79294,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-42efe6705ee01cb73f24791eb28f119e725d3ff6`;
+  const localVersion = `0.0.0-8969d1fdbde275edcbba8b006631d422e21d3057`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
