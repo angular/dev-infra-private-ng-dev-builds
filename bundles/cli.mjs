a05ad350e49b8fdcde7364b80f3b7f4f51454112
Angular Robot <angular-robot@google.com>
2023-07-10 14:45:32 +0000

    main - 1000f479 build: update dependency firebase to v10 (#1252)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -79294,7 +79294,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-75f310e5beaf10fc6698ce32334aafdb96b91829`;
+  const localVersion = `0.0.0-1000f479256aaa00847b3eecdca0436c7468af33`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
