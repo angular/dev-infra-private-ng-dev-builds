f15a4aeb96a2b55a8e04315a2b2ea9dd0768f2d1
Joey Perrott <josephperrott@gmail.com>
2023-07-14 15:09:59 +0000

    main - c1c17eae release: update circleci angular/dev-infra orb to 1.0.9 (#1276)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -79379,7 +79379,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-1ad310800a2514b948e6be7bf2202ebed16b8aba`;
+  const localVersion = `0.0.0-c1c17eae47b2e4714440a25c71ca07f3d40c84e8`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
