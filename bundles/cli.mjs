f829950b796f983b2a64b1324f0606aa83eb8656
Angular Robot <angular-robot@google.com>
2023-09-05 16:05:58 +0000

    main - 4ef0f8bf build: update actions/checkout action to v4 (#1366)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74673,7 +74673,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-b2a04c36d008d87dd40dae4aa85e8df9b569233e`;
+  const localVersion = `0.0.0-4ef0f8bfa02b4fa54cef5e1e5cf1871145b96df1`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
