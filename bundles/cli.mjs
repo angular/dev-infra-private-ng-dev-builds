09c58eaf121b8e71edbe863c373854c0a221e290
aanchal <aanchal.agarwal@knoldus.com>
2023-09-05 16:06:51 +0000

    main - f959638f docs: add the alt altribute (#1365)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74673,7 +74673,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-4ef0f8bfa02b4fa54cef5e1e5cf1871145b96df1`;
+  const localVersion = `0.0.0-f959638f1a866fca0d805013be3312e820b9183e`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
