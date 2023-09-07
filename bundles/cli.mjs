c7f93d409789bad6e7230420db15abfd70fac0b2
Joey Perrott <josephperrott@gmail.com>
2023-09-07 19:45:29 +0000

    main - 1befe6d2 feat(github-actions): allow specifying a ref during checkout (#1375)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74673,7 +74673,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-ea9c417d40a03a6a634a409d0773486828658508`;
+  const localVersion = `0.0.0-1befe6d24fd5cab818e7f8bada40ab364781b8a8`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
