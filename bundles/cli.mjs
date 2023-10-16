9c9c291515699650cabdbb66022387c527503898
Angular Robot <angular-robot@google.com>
2023-10-16 19:19:39 +0000

    main - 75d1571 build: update io_bazel_rules_sass digest to 4ce4f55 (#1435)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75084,7 +75084,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-117bb940c76c68a1a1c6b361e68efc668d0725de`;
+  const localVersion = `0.0.0-75d1571d465b8f7aa812b8955d8218a74f7287f6`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
