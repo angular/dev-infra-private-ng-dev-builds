b514e6f9de71a214ff16b1d6244461b18ac7e1c6
Jeremy Elbourn <jelbourn@google.com>
2023-10-19 15:04:55 +0000

    main - e0b4bf2 build: add npm script to build and package the build-tools package (#1452)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75090,7 +75090,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-d1d6f4e1718cbf9937ac48247e5b89d88dfa3c71`;
+  const localVersion = `0.0.0-e0b4bf2fc45aad533e3071ed1d041eda5a3a29f8`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
