3dff36069bb29e1b13caee71fcb87a88cd2f819f
Jeremy Elbourn <jelbourn@google.com>
2023-10-19 16:15:29 +0000

    main - b893be2 feat(api-gen): add rule to generate an api manifest (#1451)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75090,7 +75090,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-839e144e7092accc14171bcdf7cbcd89bda89dab`;
+  const localVersion = `0.0.0-b893be25259757dd749b06856ea9a3229280f78a`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
