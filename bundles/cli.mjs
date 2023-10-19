c806f8ffcfd63ad73615c9b56c221631e6d0dc3d
Angular Robot <angular-robot@google.com>
2023-10-19 18:05:37 +0000

    main - 3c092a0 build: update dependency jasmine to v5 (#1453)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75090,7 +75090,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-b893be25259757dd749b06856ea9a3229280f78a`;
+  const localVersion = `0.0.0-3c092a0d6eb426763cbf702bac586a448f985abc`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
