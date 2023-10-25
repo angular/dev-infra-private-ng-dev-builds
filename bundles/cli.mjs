162ca6b95d162df53acd87e90ff04972f951e14e
Jeremy Elbourn <jelbourn@google.com>
2023-10-25 15:48:26 +0000

    main - 4ccf7ee fix(api-gen): decorators aren't classes (#1467)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75090,7 +75090,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-d1471b282624e0acf9c4f611ca3b21b93122aa09`;
+  const localVersion = `0.0.0-4ccf7eed68bba0d25f7edd47997852774f61755d`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
