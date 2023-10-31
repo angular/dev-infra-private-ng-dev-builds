8e857ece2d3e1bbb60204d2b0d651ef7ed44ca8f
Jeremy Elbourn <jelbourn@google.com>
2023-10-31 15:38:43 +0000

    main - e03bff7 refactor(api-gen): use param files for all rules (#1486)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75090,7 +75090,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-e8434f2362587ea914219d8fae6e1cd81ade7183`;
+  const localVersion = `0.0.0-e03bff78a9ec32d4bb0ebab29dcfc48f02b6e848`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
