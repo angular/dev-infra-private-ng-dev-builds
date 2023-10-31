981fe0b23d3f8c6934c222fd7759d06547887643
Angular Robot <angular-robot@google.com>
2023-10-31 15:42:00 +0000

    main - 57300ee build: update dependency @angular/fire to v17 (#1485)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74352,7 +74352,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-fd5de4be257dba56d5725f9dcc0f908be3928c24`;
+  const localVersion = `0.0.0-57300ee740e0432fd270a235d2084e8f69a2072a`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
