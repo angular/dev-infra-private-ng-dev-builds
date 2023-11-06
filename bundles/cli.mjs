5debc5066c856729f7998e0676a2da4b4e7e12bf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-11-06 20:32:20 +0000

    main - 0357d62 refactor(bazel): remove ESM bundling for markdown to HTML

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74352,7 +74352,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-7c521f6f9e0f5318495895e475f9686d8ad10385`;
+  const localVersion = `0.0.0-0357d62295bfd1b967b37b4dd10b06f5e5aa6908`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
