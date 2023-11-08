2d496b87089bfb623f1a399b04af74c2b8c5a043
Angular Robot <angular-robot@google.com>
2023-11-08 12:28:16 +0000

    main - 33fd623 build: update dependency @types/yarnpkg__lockfile to v1.1.9

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74352,7 +74352,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-50f7e34658eb5c18370450532b4c7d9510ffa541`;
+  const localVersion = `0.0.0-33fd623e5b30aabb551b9d958fd64e76f500c043`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
