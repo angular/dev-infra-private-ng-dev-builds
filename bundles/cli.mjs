fe76579a9e86447a773d43f0c54dce90f3dc51a7
Jeremy Elbourn <jelbourn@google.com>
2023-11-09 16:29:26 +0000

    main - 1430f2e refactor(api-gen): remove example inlining

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74352,7 +74352,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-e559d1933eb96febccc1ea7788d32c8a8145df43`;
+  const localVersion = `0.0.0-1430f2ee57980b68ee2f0428e52577c40dabf5c9`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
