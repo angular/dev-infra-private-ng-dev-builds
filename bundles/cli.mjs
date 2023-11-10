c95d41d11d9f8cd183209a24a3deb0f98b235377
Joey Perrott <josephperrott@gmail.com>
2023-11-10 19:06:17 +0000

    main - 6032e16 feat(bazel/markdown-to-html): support doc workflows and steps in the docs markdown (#1514)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74374,7 +74374,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-27757a898abc138dcd554becc7e01565e5f4be54`;
+  const localVersion = `0.0.0-6032e16714ffb6522d495a2e4391cd4466988c21`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
