37fff0e1a2f9a14058b083f66fde464d834671c6
Angular Robot <angular-robot@google.com>
2023-11-14 14:46:36 +0000

    main - 45820c5 build: update github/codeql-action action to v2.22.6 (#1525)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74380,7 +74380,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-e2af384cb1aee2b7281fd5d78c47be08a6c6a4a3`;
+  const localVersion = `0.0.0-45820c55cb4420d77b7b53fbaf0da9538fcacace`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
