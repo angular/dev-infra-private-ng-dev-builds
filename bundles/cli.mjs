ce88945af4128dc6946476c1bcc89c747054b6aa
Joey Perrott <josephperrott@gmail.com>
2023-11-14 21:30:19 +0000

    main - 04bfb65 fix(bazel/markdown-to-html): include breadcrumbs in level 1 headers (#1529)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74380,7 +74380,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-1b702f8ef0d5edc53d59f23cadaccfbd6d2fd54b`;
+  const localVersion = `0.0.0-04bfb65cbfc2c75d1b2516f191521ff183196124`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
