a18326e3d94aac04d75f99207a0d28399d549b27
Angular Robot <angular-robot@google.com>
2023-11-16 21:18:32 +0000

    main - af6d1c3 build: update yarn to v4.0.2 (#1538)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74380,7 +74380,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-8735e171843adc66ad5ce60a24ba5a8842f61cec`;
+  const localVersion = `0.0.0-af6d1c371f387efd5703a24739412ea862c3218e`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
