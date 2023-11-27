3d42fea9e3c9cf5b2dfa239ca69c1e0f04b8fa02
Angular Robot <angular-robot@google.com>
2023-11-27 16:16:10 +0000

    main - b528b08 build: update dependency jsdom to v23 (#1545)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -74380,7 +74380,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-8d620dd288dcde4cb2122afb4eddad246ceae780`;
+  const localVersion = `0.0.0-b528b08403a21b7334eeeb795b4e5c099679b0d9`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
