581fbc0546738cf4e0ded85d2ba307ba20b781cc
Joey Perrott <josephperrott@gmail.com>
2023-11-28 19:44:14 +0000

    main - f00131a build: create karma test macro for testing angular applications in the repo (#1551)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75231,7 +75231,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-60024afe22bd467fd022322eae09ba933786547d`;
+  const localVersion = `0.0.0-f00131a6ab074797904b905c6a7637af1ce7ce80`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
