81a8bc5b8d08b4fea9e4c8f3bfe686032387edac
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-12-12 15:59:59 +0000

    main - ecd9493 test(shared-docs): implement unit tests for shared components, directives etc (#1698)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75234,7 +75234,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-d5e455773727989a24de528961da1a1dc70453f3`;
+  const localVersion = `0.0.0-ecd94935415fda77ac6d33d8f0335fe1708da223`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
