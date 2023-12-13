c4c915572998b051d3f196e55dc53cd5364b0c4d
Angular Robot <angular-robot@google.com>
2023-12-13 00:31:46 +0000

    main - d2b340f build: update octokit dependencies (#1687)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75234,7 +75234,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a3, _b2, _c2;
-  const localVersion = `0.0.0-0913ad4a2ad106bb99e47ebb556df8ea2acc5e27`;
+  const localVersion = `0.0.0-d2b340f67d87158a34b54ecb3caa797baf0626e6`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
