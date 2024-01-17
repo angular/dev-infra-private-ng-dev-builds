cdbbb64c01e02d4edda03d66fa0919504d8c5dfc
Joey Perrott <josephperrott@gmail.com>
2024-01-17 15:36:57 +0000

    main - ae9155d ci: reduce the level of enforced pull request validations for branch manager (#1750)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -80422,7 +80422,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-3a4640aba2fb4c13ce117e73a713f52cd3506ddc`;
+  const localVersion = `0.0.0-ae9155d1083fd8390e98bfe09a5ccf89f0375b52`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
