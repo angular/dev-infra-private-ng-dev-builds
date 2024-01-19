2e682effb9bed8fa31b380f4dc854777b66bb348
Angular Robot <angular-robot@google.com>
2024-01-19 16:21:20 +0000

    main - 04166b4 build: update actions/upload-artifact digest to 694cdab (#1756)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -80422,7 +80422,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-ab86d6660fcdb24b899314d4a09efe7db8a16a42`;
+  const localVersion = `0.0.0-04166b449a2faad50b7ad68d832bb78a0a24af72`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
