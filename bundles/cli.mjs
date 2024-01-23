0b8de42e74864cb49170e46e2a2f9e203f7a937f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-01-23 19:57:10 +0000

    main - f0fa701 fix(bazel): fix api-docs rendering never generating member cards

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -80422,7 +80422,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-8ae2c4439afc1b37f6160c14e7422657624c1563`;
+  const localVersion = `0.0.0-f0fa701114b57b7d633a2ab3813034ab1735e3c0`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
