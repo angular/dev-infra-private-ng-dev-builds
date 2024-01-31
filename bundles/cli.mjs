de61f540870864d9fa0ad23c3848b8db9a14d5e3
Joey Perrott <josephperrott@gmail.com>
2024-01-31 17:36:39 +0000

    main - 5254150 refactor(shared-docs): use `docs-` prefix instead of `adev-` for class names (#1771)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -80422,7 +80422,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-20822706c874a263fafcdc78bf7aefd2d47bf10c`;
+  const localVersion = `0.0.0-5254150825dd7a7bf8a86cf5aeccd31a8076dee5`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
