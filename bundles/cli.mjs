c21f93acb618bcaeda52a8065e7b6c9242def182
Joey Perrott <josephperrott@gmail.com>
2024-02-06 16:57:02 +0000

    main - c83e99a fix(shared-docs): set a new stacking context in code block elements (#1776)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -80459,7 +80459,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-5ed769e12301963aee0f16f8b210830af727c183`;
+  const localVersion = `0.0.0-c83e99a12397014162531ca125c94549db55dd84`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
