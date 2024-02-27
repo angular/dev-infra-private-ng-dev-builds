848ec2d35fcd903418ddd6fcf2487896018975e8
Matthieu Riegler <kyro38@gmail.com>
2024-02-27 14:55:13 +0000

    main - c9d5c28 fix(shared-docs): remove unwanted styling on API page. (#1815)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -80684,7 +80684,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-301473cb5fbcd6cca8b70a6369dcdf3dc2d12e08`;
+  const localVersion = `0.0.0-c9d5c2873441ba879202777822a315bae9d8ce9c`;
   const workspacePackageJsonFile = path4.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path4.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
