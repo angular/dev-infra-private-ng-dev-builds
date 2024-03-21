b4c95f3a102beba7d9632cb86e591c4790ac4bf9
Joey Perrott <josephperrott@gmail.com>
2024-03-21 15:40:18 +0000

    main - 9bf2527 refactor(shared-docs): rename markdown to pipeline (#1939)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75406,7 +75406,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-28dd48e1ad6f3bc301535bc36a9932619704d247`;
+  const localVersion = `0.0.0-9bf25278ee65772a41e2df61289882d84efbeed3`;
   const workspacePackageJsonFile = path5.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path5.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
