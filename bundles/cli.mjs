43caa47b9ea70629976ccbe2b4fb4869f202c080
Matthieu Riegler <kyro38@gmail.com>
2024-04-18 22:14:42 +0000

    main - f122b83 refactor(bazel/browsers): fix sha for macOS Firefox arm/x64 (#1990)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75499,7 +75499,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-702d5f5c2a78541d6cbb35ba758a9cf9181733bb`;
+  const localVersion = `0.0.0-f122b83d2a49e24eb9dfcd54fca9fc97612b832d`;
   const workspacePackageJsonFile = path5.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path5.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
