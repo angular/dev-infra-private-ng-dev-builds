59a21322d3454182938140ad3d803afa2c4dfc02
Angular Robot <angular-robot@google.com>
2024-04-22 21:06:23 +0000

    main - c0edda6 build: update dependency @octokit/webhooks-types to v7.5.1 (#1999)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -75636,7 +75636,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-f628b33b1f52f1ac145196a219705e97bbe9422c`;
+  const localVersion = `0.0.0-c0edda64e7fa3b9f49ff8400204b3c5e4380adad`;
   const workspacePackageJsonFile = path5.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path5.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
