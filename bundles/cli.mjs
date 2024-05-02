8da226b67186d6c3dcba9fa94f076e60159c4130
Matthieu Riegler <kyro38@gmail.com>
2024-05-02 16:25:09 +0000

    main - f1f0a94 build(shared-docs): move dependencies to peerDependencies (#2038)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -76716,7 +76716,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-f4eba9a660ebd90430ba485702de4aae29db6846`;
+  const localVersion = `0.0.0-f1f0a9469f8d2593d22499cbe2729925be3e305d`;
   const workspacePackageJsonFile = path5.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path5.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
