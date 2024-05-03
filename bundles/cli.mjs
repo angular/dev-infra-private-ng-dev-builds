939ed96fcf973a397caef37938de33ecbb93491b
Alan Agius <alanagius@google.com>
2024-05-03 11:54:28 +0000

    main - 0108dd3 fix(shared-docs): restructure dependencies (#2045)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -76716,7 +76716,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-0581ca5177f61a7dba5221c17e60522776cef714`;
+  const localVersion = `0.0.0-0108dd3e38791911fc5140593201c74b9ba2fb77`;
   const workspacePackageJsonFile = path5.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path5.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
