7ca354a9bcd3aedfb4f9452ae83bdec9c3f082a6
Joey Perrott <josephperrott@gmail.com>
2024-06-17 14:40:02 +0000

    main - 7bd6fba ci: begin checking licenses of upstream dependencies to ensure they are approved (#2119)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -76791,7 +76791,7 @@ import * as fs4 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a2, _b2, _c2;
-  const localVersion = `0.0.0-c1bd7c8deaacced1219042d52c64efc835eec578`;
+  const localVersion = `0.0.0-7bd6fbab05a4e93c63eff02fc0aa4dc219c044a9`;
   const workspacePackageJsonFile = path5.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path5.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -21,7 +21,7 @@ import { Log } from './logging.js';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-c1bd7c8deaacced1219042d52c64efc835eec578`;
+    const localVersion = `0.0.0-7bd6fbab05a4e93c63eff02fc0aa4dc219c044a9`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const workspaceDirLockFile = path.join(workspacePath, workspaceRelativeYarnLockFilePath);
     try {
