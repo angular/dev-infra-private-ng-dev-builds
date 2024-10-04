155623e5087e4a26ca1746e1a3f230eb1318124d
Angular Robot <angular-robot@google.com>
2024-10-04 14:38:53 +0000

    main - e700e51 build: update all non-major dependencies (#2363)

--- utils/version-check.js
+++ utils/version-check.js
@@ -22,7 +22,7 @@ import { Log } from './logging.js';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-a065be34d2ebe95bbd5e291934aee23eb7bc8c6c`;
+    const localVersion = `0.0.0-e700e515fdb3e8c1a9877451841f824c5ca68bb3`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const workspaceDirLockFile = path.join(workspacePath, workspaceRelativeYarnLockFilePath);
     try {
