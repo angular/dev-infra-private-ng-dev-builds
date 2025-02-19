b6497ed23d43e1dd9df8bc42b9d46f0ee5361555
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-02-19 15:04:20 +0000

    main - 0b92761 fix(github-actions): correct outputs of setup-wsl action

--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -22,7 +22,7 @@ import { Log } from './logging.js';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-0bb960e7d6fee042d01c66b682499d973cf815a5`;
+    const localVersion = `0.0.0-0b92761d7ea2baa4c497aba5ce7708d6e9ad5f46`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const workspaceDirLockFile = path.join(workspacePath, workspaceRelativeYarnLockFilePath);
     try {
