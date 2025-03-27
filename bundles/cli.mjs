fb92ae8cda5420c8e238c3618851c85ad4a98fc4
Joey Perrott <josephperrott@gmail.com>
2025-03-27 17:58:19 +0000

    main - b2cb451 ci: remove bazel setup from lint job (#2689)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -39955,7 +39955,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-e3c0efecadda0e0fbb616abcdf447c788959ca64`;
+  const localVersion = `0.0.0-b2cb451b3454a5d4b82e7e93702cee7eb0e508b8`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-e3c0efecadda0e0fbb616abcdf447c788959ca64`;
+    const localVersion = `0.0.0-b2cb451b3454a5d4b82e7e93702cee7eb0e508b8`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
