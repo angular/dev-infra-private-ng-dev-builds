681323af52dd926147cfd8201e1b88a586ef7cd2
Angular Robot <angular-robot@google.com>
2025-04-02 17:50:17 +0000

    main - 9e17ad0 build: update yarn to v4.8.1 (#2702)

--- utils/version-check.js
+++ utils/version-check.js
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-e4d4954c1056fca66831004507d25ba352b68199`;
+    const localVersion = `0.0.0-9e17ad048abf9868dc5c0cf9b74df949bd054525`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
