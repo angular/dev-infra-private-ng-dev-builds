270ac1c3814eb2c11f6952b9dc3564420c405d41
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-04-04 16:51:07 +0000

    main - e4bf37a build: speed up golden testing by parallelizing (#2714)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -39964,7 +39964,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-1ab7328cd3a3aec07767bd27f359e54e37191fcd`;
+  const localVersion = `0.0.0-e4bf37af223483ce00f9316d227fd62cd744dc4b`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-1ab7328cd3a3aec07767bd27f359e54e37191fcd`;
+    const localVersion = `0.0.0-e4bf37af223483ce00f9316d227fd62cd744dc4b`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
