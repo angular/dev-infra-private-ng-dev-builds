a9e4627eb45742d81068087a5b57a676104fe053
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-04-14 11:49:02 +0000

    main - 35131fc fix(bazel): use `RUNFILES` in http_server

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -39976,7 +39976,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-c4f7d3cdec164044284139182b709dfd4be339ed`;
+  const localVersion = `0.0.0-35131fc980ce5451fb89d8c033efc827ad39ca68`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-c4f7d3cdec164044284139182b709dfd4be339ed`;
+    const localVersion = `0.0.0-35131fc980ce5451fb89d8c033efc827ad39ca68`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
