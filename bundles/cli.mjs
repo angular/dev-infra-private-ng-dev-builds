12f9c88e06a6d235d7d3410be9df52442eb552ba
Angular Robot <angular-robot@google.com>
2025-04-22 14:17:02 +0000

    main - 9488b2f build: update all non-major dependencies (#2733)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -39976,7 +39976,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-64d0e9da9c4309a5a05f6c0a05383203a46a700d`;
+  const localVersion = `0.0.0-9488b2f6521a8178c2cdcd5c2fd7129e4e05e92b`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
