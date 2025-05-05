fe31d3807d5dc48a915da31082e76651a294f11b
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-05-05 13:04:26 +0000

    main - 3cdfd13 ci: remove `angular/.github` for auto update

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -40002,7 +40002,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-8ffaa0fe25f5c07f6b751ad05fb0113d35a3e282`;
+  const localVersion = `0.0.0-3cdfd135799bc8f76f6fa22ce2df015d45293e32`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-8ffaa0fe25f5c07f6b751ad05fb0113d35a3e282`;
+    const localVersion = `0.0.0-3cdfd135799bc8f76f6fa22ce2df015d45293e32`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
