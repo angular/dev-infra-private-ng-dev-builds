5919b777bc2e9806448a081cd0da86fdefb6010d
Angular Robot <angular-robot@google.com>
2025-05-12 10:33:56 +0000

    main - 406fa44 build: update dependency @bazel/runfiles to v6

--- utils/version-check.js
+++ utils/version-check.js
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-43588389bd7d5f3b893ee43dbedbb6ad3eebdff8`;
+    const localVersion = `0.0.0-406fa44140d4e3e2cc508597f8800a5b37d8e009`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
