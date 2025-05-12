1c4ab5db67425dc52b9bff25660dbf5e105a4d75
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-05-12 17:32:52 +0000

    main - 1685f0a fix(github-actions): more `cross-repo angular dependencies` fixes

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -40115,7 +40115,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-82a6e3d4d398db1a6a4aa225509d3bbed98b6399`;
+  const localVersion = `0.0.0-1685f0afc967fb7de4b46ccb22f22d779ee3086c`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
