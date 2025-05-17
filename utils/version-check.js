5ac378c9b4432f15d95e53dec8b31e5c5dfc6146
Angular Robot <angular-robot@google.com>
2025-05-17 08:51:01 +0000

    main - b4fbf04 build: update github/codeql-action action to v3.28.18

--- utils/version-check.js
+++ utils/version-check.js
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-26c5768cad7c6207d78ba5b44e2db8ef049f807b`;
+    const localVersion = `0.0.0-b4fbf04a6770cf0fc4be514c13f6529ea76122a1`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
