c3ddb5562a644e61df2d8da6c7d4af4cd125cd76
Angular Robot <angular-robot@google.com>
2025-05-19 11:41:13 +0000

    main - bff2afd build: update all non-major dependencies

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -40123,7 +40123,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-5fda3087aac535f7fc9988f97fe7848fc56e7988`;
+  const localVersion = `0.0.0-bff2afd1d18e57298fcee8ecb55435595fedd3af`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-5fda3087aac535f7fc9988f97fe7848fc56e7988`;
+    const localVersion = `0.0.0-bff2afd1d18e57298fcee8ecb55435595fedd3af`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
