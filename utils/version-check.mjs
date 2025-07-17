385f44abcf5e2a2180306e45db85a806fe8ce059
Joey Perrott <josephperrott@gmail.com>
2025-07-17 19:14:15 +0000

    main - 64e129b fix(bazel/api-golden): allow api golden tooling to handle single golden files (#2900)

--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-872ab8f2d107ccab872b64e94d0e9c7c39fc9a2e`;
+    const localVersion = `0.0.0-64e129b9b9b27fc54b1e6e08d012d0d4fd2ed798`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
