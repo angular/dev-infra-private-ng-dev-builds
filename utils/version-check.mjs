28aaeb2e026807264807822eac349d8de91f98ac
Angular Robot <angular-robot@google.com>
2025-07-21 13:55:44 +0000

    main - 75b35ce build: update all non-major dependencies (#2902)

--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-02cab9e1336b956654ae958553b0577dd71d1052`;
+    const localVersion = `0.0.0-75b35ce12166f2e4b4d8e61f83d246248f0f852b`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
