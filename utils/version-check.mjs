ef1336a6a81349fc0610bf80d8c1725c985de9a5
Joey Perrott <josephperrott@gmail.com>
2025-08-05 16:38:35 +0000

    main - 6a526a9 refactor(bazel): drop interop support for api golden infra (#2924)

--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-0dd93abfd15a503b36ba6df22b0fe0c3868af93c`;
+    const localVersion = `0.0.0-6a526a9e082b8f34651d7e63774e9353d2ae9416`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
