7420f312406784d0fd9eed6a37066305f3a37c61
Joey Perrott <josephperrott@gmail.com>
2025-08-06 15:51:32 +0000

    main - d0b29e1 refactor(bazel): drop interop support for http_server (#2930)

--- utils/version-check.mjs
+++ utils/version-check.mjs
@@ -23,7 +23,7 @@ import { tryGetPackageId } from '@pnpm/dependency-path';
  */
 export async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-7cec4e89e192a5591527b5c3e2e08f62b0a55fc4`;
+    const localVersion = `0.0.0-d0b29e134f5a1de44e7cc60633688ad6dc0780bb`;
     const workspacePackageJsonFile = path.join(workspacePath, workspaceRelativePackageJsonPath);
     const pnpmLockFile = path.join(workspacePath, 'pnpm-lock.yaml');
     const yarnLockFile = path.join(workspacePath, 'yarn.lock');
