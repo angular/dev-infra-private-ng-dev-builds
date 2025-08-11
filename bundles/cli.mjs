865ef057c3dcfec521a228384dd86a14b640ee9b
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-11 13:56:49 +0000

    main - ef64fab fix(ng-dev): downgrade `@google-cloud/spanner` to `8.0.0`

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -62243,7 +62243,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-f04b129e644d93c88850471c1118224a1560b1db`;
+  const localVersion = `0.0.0-ef64fabeab088c2d016508c9b045c122cdc44116`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
