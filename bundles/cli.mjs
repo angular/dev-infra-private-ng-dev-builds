2dacfadb124940504473ef3c14da161b667a2306
Joey Perrott <josephperrott@gmail.com>
2025-08-11 13:41:49 +0000

    main - f04b129 build: remove references to //:npm_package which no longer exists

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -62243,7 +62243,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-8f19310a6861d54d57a9c98c5419a898628eaba9`;
+  const localVersion = `0.0.0-f04b129e644d93c88850471c1118224a1560b1db`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
