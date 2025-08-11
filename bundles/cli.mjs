fd061709ac7c6234e87fbb9b61f5e345573a0f0f
Joey Perrott <josephperrott@gmail.com>
2025-08-11 19:00:54 +0000

    main - a7e9f67 build: update dependency typescript to v5.9.2 (#2956)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -62253,7 +62253,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-b4adb2e222b92986a45f72bcc58365a474052ba5`;
+  const localVersion = `0.0.0-a7e9f67907ceb47ebf478b5aa0bb964edafe5ef2`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
