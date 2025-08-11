1f2685563020103dfde165abe99653bb1a60240d
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-11 09:17:52 +0000

    main - dadb2ea build: correctly replace `SCM_HEAD_SHA` with `BUILD_SCM_COMMIT_SHA`

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -62243,7 +62243,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-{BUILD_SCM_COMMIT_SHA}`;
+  const localVersion = `0.0.0-dadb2ea2c1f5a082596040372a538b03f8829d4a`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
