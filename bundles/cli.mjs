b13e94382b96441333448e4f7086fed6723802d4
Joey Perrott <josephperrott@gmail.com>
2025-08-11 18:09:51 +0000

    main - b869b4d build: use pnpm to note that ng-dev is a script in update-generated-files script (#2955)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -62243,7 +62243,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib7());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-e4b3033dfb716c2de20eaca31c416a06a5b4e718`;
+  const localVersion = `0.0.0-b869b4d8c8f1e0d88e868f4c62138d922a7946e9`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
