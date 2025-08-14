d358d8cae0787966548e43d5e3d0861eb009eb98
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-14 08:20:05 +0000

    main - 0c3f8e6 fix(github-actions): use ubuntu instead of debian

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -59052,7 +59052,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-11fa17f8e7ba38734d6f91c575b3878b9afadc27`;
+  const localVersion = `0.0.0-0c3f8e6cce68ea7510e031e106e1176f9f853762`;
   const workspacePackageJsonFile = path7.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path7.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path7.join(workspacePath, "yarn.lock");
