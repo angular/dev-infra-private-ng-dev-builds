b9bae2cc9b96652721067a69ece7e79dba123787
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-14 13:52:09 +0000

    main - 7a11f99 fix(github-actions): add module type to github actions package.json

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -59052,7 +59052,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-16e272eaa88efe5891e7e6c8e13b956ae7e5a73e`;
+  const localVersion = `0.0.0-7a11f99c467ca5ae4411c27beeec4300e32b616a`;
   const workspacePackageJsonFile = path7.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path7.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path7.join(workspacePath, "yarn.lock");
