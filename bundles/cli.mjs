93fe86fed6afb1e19a3edb518b1f09de6f9305b4
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-28 15:52:10 +0000

    main - ae4e348 ci: pin `@google-github-actions/auth` (#3016)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -58718,7 +58718,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-7b1e8511b98a7eb2908ba881fdd8be628d39fd28`;
+  const localVersion = `0.0.0-ae4e34835e566bbcc12bec8f2191b069f307ece2`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
