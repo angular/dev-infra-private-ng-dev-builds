819b0b44e5ff6f97bf7457ca590bc09a5c90336b
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-29 13:39:55 +0000

    main - 0e978b9 feat(github-actions): add cache-dependency-path to checkout-and-setup-node (#3023)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -58718,7 +58718,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-43fc44abc29c75ca2980872be78ae10cf717d29d`;
+  const localVersion = `0.0.0-0e978b9b9711efa5fd9698191aaaa25c75710f9d`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
