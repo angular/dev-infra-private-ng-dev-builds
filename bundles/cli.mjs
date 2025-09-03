586406bccb7ae283f803f1c36e127b7b16c942ce
Joey Perrott <josephperrott@gmail.com>
2025-09-03 15:18:05 +0000

    main - 0cabf62 feat(bazel): provide the node loader to load esm files in jasmine tests (#3025)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -58718,7 +58718,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-fc1176a1a61fbabca534b448ef84a7bacf28a665`;
+  const localVersion = `0.0.0-0cabf621c7fdbdf689d2d467ac2660020c179909`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
