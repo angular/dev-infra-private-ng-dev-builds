0f5934d67f4a5d3e1ef892bfd47b017c15a4cd2e
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-09-11 14:44:04 +0000

    main - 64e2aae ci: limit update frequency of github/codeql-action (#3052)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -53179,7 +53179,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-f8c8b3e42d42bd04c96b58e803064be0ec5957d2`;
+  const localVersion = `0.0.0-64e2aae8f049c699bdac7e84212dd1eee3fb0248`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
