66e00a7dd6dbbad1e6090c562821448fd87c4cc4
Angular Robot <angular-robot@google.com>
2025-09-13 06:16:06 +0000

    main - b4aee06 build: update pnpm to v10.16.0 (#3060)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -53172,7 +53172,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-36b760e0ca4279e654aeeb63fa2fae680c0650a1`;
+  const localVersion = `0.0.0-b4aee066684a99bfaadb4c23a535189d0aa39159`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
