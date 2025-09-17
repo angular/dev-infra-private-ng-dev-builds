ea19f57120053b984364eda6211f337b2756b190
Angular Robot <angular-robot@google.com>
2025-09-17 19:15:06 +0000

    main - ea29aaa build: update cross-repo angular dependencies to v21.0.0-next.4 (#3075)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56790,7 +56790,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-02baff3136415588c67b1c658f6fe8d48acc664a`;
+  const localVersion = `0.0.0-ea29aaa1017454533a029dd555ad9f93d381d100`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
