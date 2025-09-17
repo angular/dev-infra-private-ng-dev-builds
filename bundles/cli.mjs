e80bd66126295cfbe6cb2bf1aecc1108651a5265
Joey Perrott <josephperrott@gmail.com>
2025-09-17 17:03:05 +0000

    main - 59be37d build: remove previous method for setting up depending on dev-infra in bazel (#3074)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56790,7 +56790,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-00436d3805b6ee23734f827f85b687fce686f61c`;
+  const localVersion = `0.0.0-59be37dc805d3ca8158860b3e19db7934b5aa90d`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
