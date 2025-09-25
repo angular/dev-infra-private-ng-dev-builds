ba4f5dcc0e6f440a1a23f3b9f14867d193e38d73
Joey Perrott <josephperrott@gmail.com>
2025-09-25 20:39:36 +0000

    main - 5d5b928 fix(github-actions): set the number of flaky test attempts on CI RBE executions (#3097)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56793,7 +56793,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-08d451f9b4e900928b65cd31234693ef9a994492`;
+  const localVersion = `0.0.0-5d5b92883929731c9914bcbe77f78cda23be3625`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
