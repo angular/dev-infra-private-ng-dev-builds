e162324c6d6c9b4d712eeb9b786937165c1f310e
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-09-26 19:17:53 +0000

    main - 00cef30 Revert "ci: temporarily disable rules_angular updates on patch branches (#3109)"

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56793,7 +56793,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-99cb49d8f4240fcd51f93c6f05f52c869dd50d9a`;
+  const localVersion = `0.0.0-00cef30761644cb8c8e75ea448e958e4eeed3aa7`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
