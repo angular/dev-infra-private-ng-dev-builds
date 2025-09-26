05831615b5579240d8407d44e30bcc3a42d77d10
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-09-26 16:26:52 +0000

    main - 99cb49d build: update lock file (#3110)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56793,7 +56793,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-7a27dd699d98ae82593dd7eb3b4b8f0720aa530b`;
+  const localVersion = `0.0.0-99cb49d8f4240fcd51f93c6f05f52c869dd50d9a`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
