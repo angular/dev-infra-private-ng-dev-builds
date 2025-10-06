8563c57a325f4fc00b2919db05538942f9c8915d
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-06 09:22:28 +0000

    main - 58610ea test: update all integration tests to use pnpm instead of yarn (#3127)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56793,7 +56793,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-fbecf8a00ea3b49b7d4028c9f88fc7775d274a07`;
+  const localVersion = `0.0.0-58610eacb7efbc261f16197586fa7fb4c81adccd`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
