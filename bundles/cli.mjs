b07a986fd679ce94afcb60e915d49e5e922feb19
Angular Robot <angular-robot@google.com>
2025-10-07 10:29:25 +0000

    main - 4c4cdfe build: update dependency karma-jasmine to v5 (#3133)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56846,7 +56846,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-afc75dff395220ac31d6d9b94b2fc6c53c36cfa5`;
+  const localVersion = `0.0.0-4c4cdfe87dd898355bd2762fdd0ee3d899346a3b`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
