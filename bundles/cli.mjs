ebeff597a41d8bcd785df182b8aac8eb1a1f934d
Angular Robot <angular-robot@google.com>
2025-10-07 10:29:43 +0000

    main - 5c1d596 build: update bazel dependencies (#3118)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56846,7 +56846,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-4c4cdfe87dd898355bd2762fdd0ee3d899346a3b`;
+  const localVersion = `0.0.0-5c1d5964db0faa88a7071384c266683fafcb1755`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
