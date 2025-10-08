650ac6faed7c4d83a70a7d9418f30bd383c520cb
Angular Robot <angular-robot@google.com>
2025-10-08 10:59:46 +0000

    main - 26b2c12 build: update pnpm/action-setup action to v4.2.0 (#3136)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56846,7 +56846,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-671cfe69504219099cff0778c46b7687558dfd1c`;
+  const localVersion = `0.0.0-26b2c129c4e494126cc9c42d10a273a26e766244`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
