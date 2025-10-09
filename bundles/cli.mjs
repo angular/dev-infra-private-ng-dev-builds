e877c7e4fe14a67d425f5af705bf583fbbba9967
Angular Robot <angular-robot@google.com>
2025-10-09 10:57:23 +0000

    main - 56543b1 build: update rules_browsers digest to 0e04a54 (#3142)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56846,7 +56846,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-0805ed4db19ab9d5bc673f7fe97623c683fc92a9`;
+  const localVersion = `0.0.0-56543b10ddb9941c76b4413cc24ec3fa7a43cf5c`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
