5cd66227d38bb0feed0989254757ca51077a8607
Angular Robot <angular-robot@google.com>
2025-10-16 16:16:17 +0000

    main - c776985 build: update cross-repo angular dependencies (#3156)

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -56876,7 +56876,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-1fd3f8b3fa8d9c438135bc6c5cecacd07920ef42`;
+  const localVersion = `0.0.0-c776985eeff8f041f142d85577210976c98a2922`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
