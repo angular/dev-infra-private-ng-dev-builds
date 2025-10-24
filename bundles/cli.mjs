616fedd5ae1655bb227f61c67fc9945ec75d3350
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-24 18:23:01 +0000

    main - db74b80 ci: correct schedule syntax in renovate preset

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -49863,7 +49863,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-e17731ca1da0c62d7de5fd72fc4541d363946fa4`;
+  const localVersion = `0.0.0-db74b80bd7f233f465f6ac89b2093e01ea7ffbf0`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
