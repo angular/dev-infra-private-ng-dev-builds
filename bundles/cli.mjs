aff0360611f6ef466b06a5078447d9b4208bc6fb
Angular Robot <angular-robot@google.com>
2025-10-27 12:28:06 +0000

    main - df29349 build: update actions/upload-artifact action to v5

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -49863,7 +49863,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-64a6065b7aad5a23503c1a8b0a0cc94a02185a03`;
+  const localVersion = `0.0.0-df29349bcc32ac02c6966c1f986b14b0571c6f42`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
