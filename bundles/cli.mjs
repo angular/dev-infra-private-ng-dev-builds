2670fc608b9bd9dc0c3ad499afef03810ce019af
Angular Robot <angular-robot@google.com>
2025-10-30 10:13:46 +0000

    main - a381508 build: update ghcr.io/puppeteer/puppeteer docker tag to v24.27.0

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -49877,7 +49877,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-cb26566cfe0d23cdb6d44c72033695b7b9a11dcc`;
+  const localVersion = `0.0.0-a381508f80790e6ea45fda0e32686c3485ef4b3f`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
