f3ab28388242df0aa4f44c8da303833914168118
Angular Robot <angular-robot@google.com>
2025-11-03 21:41:58 +0000

    main - 64fbfea build: update octokit monorepo

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -49892,7 +49892,7 @@ import * as fs3 from "fs";
 import lockfile from "@yarnpkg/lockfile";
 var import_dependency_path = __toESM(require_lib8());
 async function verifyNgDevToolIsUpToDate(workspacePath) {
-  const localVersion = `0.0.0-3d0d6246402f2f4621aeee302290f77ac2c4e397`;
+  const localVersion = `0.0.0-64fbfea9823cf54d2caa96753ae0cb0745736f01`;
   const workspacePackageJsonFile = path6.join(workspacePath, workspaceRelativePackageJsonPath);
   const pnpmLockFile = path6.join(workspacePath, "pnpm-lock.yaml");
   const yarnLockFile = path6.join(workspacePath, "yarn.lock");
