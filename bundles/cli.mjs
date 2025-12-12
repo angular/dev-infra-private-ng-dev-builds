804aa82b2aaad6c4d763dbca75c2ca3b4db9b3a4
Angular Robot <angular-robot@google.com>
2025-12-12 17:19:16 +0000

    main - f1be871 build: update cross-repo angular dependencies to v21.1.0-next.3

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -48884,7 +48884,7 @@ var import_yaml3 = __toESM(require_dist());
 import * as path6 from "path";
 import * as fs3 from "fs";
 var import_dependency_path = __toESM(require_lib8());
-var localVersion = `0.0.0-ebda1fa20dd99798b742625770b06acb7b66435d`;
+var localVersion = `0.0.0-f1be87109c769d1c641deccae563178dc92aac37`;
 var verified = false;
 async function ngDevVersionMiddleware() {
   if (verified) {
