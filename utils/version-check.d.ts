bb9edb8586a5aff6337e326570bb4ba6cd0e4670
Angular Robot <angular-robot@google.com>
2026-01-29 15:21:58 +0000

    main - cedbaa1 build: update all non-major dependencies

--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-5e2e5b9c0882a743939144945a161da9eb11209e";
+export declare const localVersion = "0.0.0-cedbaa1118bd1217c055283d6e49643321fc6748";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
