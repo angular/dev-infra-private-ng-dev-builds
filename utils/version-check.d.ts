b2466b62e05ac6b9d9a7f43320df2559c7ffd87a
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-02-03 17:47:15 +0000

    main - 8573a67 ci: add setup-uv action to ng-renovate workflow

--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-6fa596ff434e3cabe6d7deb90fbb611304837b47";
+export declare const localVersion = "0.0.0-8573a67713a8f4edb701198b7098afdae4035920";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
