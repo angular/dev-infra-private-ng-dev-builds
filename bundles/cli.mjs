371c6ad14109c6e2976935e3dcce87fcf1c8ed06
Angular Robot <angular-robot@google.com>
2026-02-12 18:35:25 +0000

    main - d688d95 build: update bazel dependencies

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -49013,7 +49013,7 @@ var import_yaml3 = __toESM(require_dist());
 import * as path7 from "path";
 import * as fs4 from "fs";
 var import_dependency_path = __toESM(require_lib8());
-var localVersion = `0.0.0-c1c3bed97f726e949f0fdb4f5a80e5ddea6b680d`;
+var localVersion = `0.0.0-d688d95ef7122aeed5a02a6e6dea8670bb782978`;
 var verified = false;
 async function ngDevVersionMiddleware() {
   if (verified) {
--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-c1c3bed97f726e949f0fdb4f5a80e5ddea6b680d";
+export declare const localVersion = "0.0.0-d688d95ef7122aeed5a02a6e6dea8670bb782978";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
