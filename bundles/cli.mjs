0b2c74dc030ce2a3e9062b2680b1d370235156f8
Angular Robot <angular-robot@google.com>
2026-02-24 15:32:21 +0000

    main - 831fe0a build: update cross-repo angular dependencies

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -49610,7 +49610,7 @@ var import_yaml3 = __toESM(require_dist());
 import * as path7 from "path";
 import * as fs4 from "fs";
 var import_dependency_path = __toESM(require_lib8());
-var localVersion = `0.0.0-7d134480b5181fdff66714541d6cde1497089363`;
+var localVersion = `0.0.0-831fe0a8f23c918e625115aff92d8d93633b7d62`;
 var verified = false;
 async function ngDevVersionMiddleware() {
   if (verified) {
--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-7d134480b5181fdff66714541d6cde1497089363";
+export declare const localVersion = "0.0.0-831fe0a8f23c918e625115aff92d8d93633b7d62";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
