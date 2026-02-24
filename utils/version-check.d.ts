52b11c74bb76310a1eb8dc7ef5fff11961646b84
Angular Robot <angular-robot@google.com>
2026-02-24 15:31:38 +0000

    main - ff583c1 build: update ghcr.io/puppeteer/puppeteer docker tag to v24.37.5

--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-0cca77ad76933e3c4123212282dce4cfb794392b";
+export declare const localVersion = "0.0.0-ff583c102d9a698107bf1aa33970e42f2af83ed7";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
