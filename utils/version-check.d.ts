31c44c9199c18f41706ddc8e4f493e367d457495
Angular Robot <angular-robot@google.com>
2026-06-04 23:41:19 +0000

    "main - d96c1cb build: lock file maintenance"

--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-7577392319a308e9e197271693ffab75fdd5b636";
+export declare const localVersion = "0.0.0-d96c1cbfdd1dc3faf6a59b0175a7fb8bb3cbace7";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
