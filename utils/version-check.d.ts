e2db05352882d2ffd6f299aedc949c3b523aea61
Angular Robot <angular-robot@google.com>
2026-06-08 14:33:01 +0000

    "main - 38dab41 build: update rules_browsers browsers metadata to latest versions"

--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-629f3758d56351b40e6becc6fa7962a652577a4d";
+export declare const localVersion = "0.0.0-38dab41ae1137ccf558935c8184880fd5e9f571c";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
