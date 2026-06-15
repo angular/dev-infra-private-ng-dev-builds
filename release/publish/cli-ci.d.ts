cefe10420ad2ecdadd21129be75b71aa485baea6
Joey Perrott <josephperrott@gmail.com>
2026-06-15 15:46:01 +0000

    "main - 023fc8f fix(github-actions): format publish-ci.ts and update main.js bundle"

--- release/publish/cli-ci.d.ts
+++
@@ -1,7 +0,0 @@
-import { CommandModule } from 'yargs';
-export interface ReleasePublishCiOptions {
-    builtPackagesDir: string;
-    expectedSha: string;
-    dryRun?: boolean;
-}
-export declare const ReleasePublishCiCommandModule: CommandModule<{}, ReleasePublishCiOptions>;
--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-f3270e93a975213954aed59456399fc32d33c6a3";
+export declare const localVersion = "0.0.0-023fc8fd55661b2ba24151350bd7460dafd8ae88";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
