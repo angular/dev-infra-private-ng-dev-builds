21f0f554cdc80196427a7b024fc58f583bca9476
Angular Robot <angular-robot@google.com>
2026-07-06 07:24:07 +0000

    "main - fe36dd7 build: update all non-major dependencies"

--- bundles/cli.mjs
+++ bundles/cli.mjs
@@ -36493,7 +36493,7 @@ var import_yaml4 = __toESM(require_dist());
 import * as path5 from "path";
 import * as fs4 from "fs";
 var import_dependency_path = __toESM(require_lib5());
-var localVersion = `0.0.0-1b83fcd99cec9ff01e539f293c643ee962adc217`;
+var localVersion = `0.0.0-fe36dd7d7c40ab8241dcc5abe6d129b5ab747a10`;
 var verified = false;
 async function ngDevVersionMiddleware() {
   if (verified) {
--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-1b83fcd99cec9ff01e539f293c643ee962adc217";
+export declare const localVersion = "0.0.0-fe36dd7d7c40ab8241dcc5abe6d129b5ab747a10";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
