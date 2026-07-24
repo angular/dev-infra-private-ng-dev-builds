702adb83c531475f85b7dd1da445c0b89244ba1b
Angular Robot <angular-robot@google.com>
2026-07-24 10:01:36 +0000

    "main - a7e63c1 build: update astral-sh/setup-uv action to v9"

--- utils/version-check.d.ts
+++ utils/version-check.d.ts
@@ -1,3 +1,3 @@
-export declare const localVersion = "0.0.0-49206eb8046cb4dfc8969b4d98f84323383a3ee4";
+export declare const localVersion = "0.0.0-a7e63c1d56e5b4613ef69ded8e0d8f3849a25e10";
 export declare function ngDevVersionMiddleware(): Promise<void>;
 export declare function verifyNgDevToolIsUpToDate(workspacePath: string): Promise<boolean>;
